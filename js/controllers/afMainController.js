95ba806f4f3a26671a0b08896d66f8f88b41afe0
Arthur <arthur.p.wright@gmail.com>
2015-08-10 15:33:59 -0400

    Set up angular to hit api and filter based on date taken

---
+++ js/controllers/afMainController.js
@@ -0,0 +1,18 @@
+angular.module('AngularFlickr').controller('AfMainController', function(FlickrAPI, $scope, $filter){
+  $scope.photos = [];
+  var orderBy = $filter('orderBy');
+  FlickrAPI.flickrPhotos()
+  .then(function(data){
+    var photos = data.photo;
+    for (var i = 0; i < photos.length; i++) {
+      var photo = photos[i];
+      FlickrAPI.photoData(photo.id, photo.secret).then(function(data){
+        data.timeInMS = Date.parse(data.dates.taken);
+        $scope.photos.push(data);
+      });
+    }
+  });
+  $scope.order = function(predicate, reverse) {
+    $scope.photos = orderBy($scope.photos, predicate, reverse);
+  };
+});
